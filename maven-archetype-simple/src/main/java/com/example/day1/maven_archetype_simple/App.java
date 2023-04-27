3f226299ccccaf414dd50794676921feab6f95ee
YogeshKumar25 <117429291+YogeshKumar25@users.noreply.github.com>
2023-04-27 15:37:58 +0530

    Add files via upload

---
+++ maven-archetype-simple/src/main/java/com/example/day1/maven_archetype_simple/App.java
@@ -0,0 +1,31 @@
+package com.example.day1.maven_archetype_simple;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver=new EdgeDriver();
+        driver.get("https://demo.opencart.com/index.php?route=account/register&language=en-gb");
+        driver.findElement(By.xpath("//*[@id=\"input-firstname\"]")).sendKeys("yogesh");
+        driver.findElement(By.id("input-lastname")).sendKeys("Sekar");
+
+        driver.findElement(By.cssSelector("#input-email")).sendKeys("jagapraveen26@gmail.com");
+
+//        driver.findElements(By.xpath("//*[@id=\"input-password\"]")).sendKeys("Jaga12345");
+
+        driver.findElement(By.xpath("//*[@id=\"input-password\"]")).sendKeys("Jaga123");
+
+        Thread.sleep(5000);
+
+        driver.findElement(By.xpath("//*[@id=\"form-register\"]/div/div/div/input")).click();
+
+        driver.findElement(By.xpath("//*[@id=\"form-register\"]/div/div/button")).click();
+    }
+}
